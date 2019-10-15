67dabb2d36576aeacde7e3d0f65aca5dcfac708c
Rachapalli Swapnapriya <rachapalli.swapn@outlook.com>
2019-10-15 17:09:35 +0530

    Initial Project

---
+++ MVC_GithubTest_1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_GithubTest_1.Startup))]
+namespace MVC_GithubTest_1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
